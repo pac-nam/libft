dc5231d167a2ed5bc9ba639e48c4599b5414c789
pac-nam <thomasbleuse02@gmail.com>
2018-07-06 18:31:30 +0200

    problem == solved

--- char/ft_isspace.c
+++ char/ft_isspace.c
@@ -12,7 +12,7 @@
 
 int		ft_isspace(int c)
 {
-	if (c == '\t' || c == '\n' || c = '\v' ||
+	if (c == '\t' || c == '\n' || c == '\v' ||
 			c == '\f' || c == '\r' || c == ' ')
 		return (1);
 	return (0);
