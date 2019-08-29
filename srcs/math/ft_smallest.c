24a978ef149b269a32a7dac86ba39112aab569cb
tbleuse <t.bleuse@capitaldata.fr>
2019-08-29 14:40:55 +0200

    ft_smallest added

--- srcs/math/ft_smallest.c
+++ srcs/math/ft_smallest.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   math.h                                             :+:      :+:    :+:   */
+/*   ft_smallest.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/08 13:54:05 by tbleuse           #+#    #+#             */
-/*   Updated: 2018/03/07 11:05:16 by tbleuse          ###   ########.fr       */
+/*   Created: 2018/03/30 13:46:17 by tbleuse           #+#    #+#             */
+/*   Updated: 2018/03/30 18:07:16 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATH_H
-# define MATH_H
+/*
+**	ft_smallest return the smallest int of the two params.
+*/
 
-int		ft_random(void);
-int		ft_sqrt(int nb);
-int		ft_bracket(char *str);
-int		ft_power(int number, int power);
-int		*ft_sort_int_tab(int *tab, size_t length);
-int		ft_abs(int nb);
-int     ft_biggest(int a, int b);
-int     ft_smallest(int a, int b);
-
-#endif
+int ft_smallest(int a, int b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
