ca3725d79fe3621fd05f5696afde6214eeaefd9f
pacman <tbleuse@student..42.fr>
2019-09-28 16:13:33 +0200

    structures of malloc hiddened

--- includes/malloc_srcs.h
+++ includes/malloc_srcs.h
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   malloc.h                                           :+:      :+:    :+:   */
+/*   malloc_srcs.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,18 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MALLOC_H
-# define MALLOC_H
+#ifndef MALLOC_SRCS_H
+# define MALLOC_SRCS_H
 
+# include <unistd.h>
 # include <stdlib.h>
 
-void					ft_free(void *ptr);
-void					*ft_malloc(size_t size);
-void					*ft_realloc(void *ptr, size_t size);
-void					ft_show_alloc_mem(void);
-void					*ft_calloc(size_t nmemb, size_t size);
-void					*ft_reallocf(void *ptr, size_t size);
-size_t					ft_malloc_good_size(size_t size);
-size_t					ft_malloc_size(void *ptr);
+typedef	struct			s_cluster
+{
+	int					freesize;
+}						t_cluster;
+
+typedef	struct			s_block
+{
+	size_t				size;
+	struct s_block		*next;
+}						t_block;
+
+typedef struct			s_context
+{
+	t_block				*tiny;
+	t_block				*small;
+	t_block				*large;
+}						t_context;
+
+t_context				g_alloc;
+
+t_block					*ft_get_malloc_page(void *ptr);
+int						ft_check_ptr(t_block *page, void *to_find);
+
+# define TINY	1024
+# define SMALL	1024*128
+# define LARGE	-1
+# define ALIGN	16
+# define PAGESIZE getpagesize()
+# define BLOCKSIZE ft_malloc_good_size(sizeof(t_block))
+# define CLUSTERSIZE ft_malloc_good_size(sizeof(t_cluster))
+# define CS CLUSTERSIZE
 
 #endif
